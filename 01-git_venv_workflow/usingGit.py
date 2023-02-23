7946bde5e18a64dfdf0df49dca70c72689f279cb
Darryl Hardy <9351714+flippind@users.noreply.github.com>
2023-02-23 13:49:54 -0700

    usingGit v1

---
+++ 01-git_venv_workflow/usingGit.py
@@ -0,0 +1,8 @@
+#usingGit.py
+
+print("Set your working path to the root project folder\n\tcd projectRootPath")
+print('Initialize the repo\n\tgit init')
+print("Make a file change")
+print("Add files for tracking\n\tgit add fileName")
+print('Create a new commit\n\tgit commit')
+
